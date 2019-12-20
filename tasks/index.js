8d46564b99926d88702a0063209fe7b74693cf4f
Steen Klingberg <steen@sk-ab.se>
2019-12-20 13:06:28 +0100

    updated freemarker

--- tasks/index.js
+++ tasks/index.js
@@ -11,9 +11,10 @@ module.exports = function(grunt) {
         let temp = path.resolve(__dirname, '../../../.temp.log');
 
         let cmd = 'java -jar ' + path.resolve(__dirname, 'lib/fmpp.jar') + ' --output-format=HTML -C ' + 
-            path.resolve(__dirname, '../../..', config) + ' > ' + temp;
+            path.resolve(__dirname, '../../..', config);
+        console.log(cmd);
 
-        execSync(cmd);
+        execSync(cmd  + ' > ' + temp);
         console.log(fs.readFileSync(temp).toString());
         fs.unlink(temp, (err) => {
             if (err) throw err;
